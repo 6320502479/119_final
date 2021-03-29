a8328ab716df28a08cc3f80f36ad844c64798d55
6320502479 <pattrapol.J@ku.th>
2021-03-29 18:23:51 +0700

    h

--- final_1.c
+++ final_1.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
-#include <math.h>
 int main()
 {
     int k,n,ck,i;
-    scanf("%d %d",&k,&n);
-    while(i<32750)
+    scanf("%d",&k);
+    scanf("%d",&n);
+    if(k>n && n<10)
+    {
+        while(i<32750)
         {
             k++;
             if(k%10==n)
                 break;
         }
         printf("%d",k);
+    }
+    return 0;
 }
